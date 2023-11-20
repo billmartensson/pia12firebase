2d1ea37db49abd5a430d637ba6aebd18e7418a79
Bill Mårtensson <bill@magictechnology.se>
2023-11-20 09:19:30 +0100

    Initial Commit

---
+++ pia12firebase/pia12firebaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  pia12firebaseApp.swift
+//  pia12firebase
+//
+//  Created by BillU on 2023-11-20.
+//
+
+import SwiftUI
+
+@main
+struct pia12firebaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
